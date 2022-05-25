5265a7e951efe874a53e2e096acaa41fdeefd897
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-25 16:03:56 +0200

    database: dischi

---
+++ database.php
@@ -0,0 +1,29 @@
+<?php
+    $dischi = [
+        [
+            'nome'=> 'New Jersey',
+            'artista'=> 'Bon Jovi',
+            'anno' => 1988
+        ],
+        [
+            'nome'=> 'Live at Wembley 86',
+            'artista'=> 'Queen',
+            'anno' => 1992
+        ],
+        [
+            'nome'=> "Ten's summoner's tales",
+            'artista'=> 'Sting',
+            'anno' => 1993
+        ],
+        [
+            'nome'=> 'Steve gadd band',
+            'artista'=> 'Steve gadd band',
+            'anno' => 2018
+        ],
+        [
+            'nome'=> 'Bad',
+            'artista'=> 'Michael Jacjson',
+            'anno' => 1987
+        ],
+    ];
+?>
